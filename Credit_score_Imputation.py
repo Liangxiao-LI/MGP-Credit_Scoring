3f3af96dee8a030a05cf140a97131686c633d594
BL-Starlord <1021618717@qq.com>
2024-02-16 11:47:47 +0000

    XGB hyperparameter tuning + Imput/Output modification

--- Credit_score_Imputation.py
+++ Credit_score_Imputation.py
@@ -5,10 +5,23 @@ import seaborn as sns
 
 #!Row10 change input dataset, Row65 change randomforest hyperparameter, Row 141 change XGB hyperparameter
 
+#%% Input/Output modification
+
+#The filename for the data
+inputfile = 'data.csv'
+#The file name to store the xbg regressor after training
+regressor_filename = 'xgb_regressor_1.joblib'
+#The file name to store the parameter used for training
+xgb_train_parameter_filename = 'xgb_train_parameter_1.joblib'
+
+#%%
+
 # Load the CSV data into a DataFrame
 #!Change this part to modify the imput csv file
-df = pd.read_csv('data.csv')
-#df = pd.read_excel('CleanedData4.xlsx', engine='openpyxl')
+if inputfile.split('.')[-1] == 'csv':
+    df = pd.read_csv(inputfile)
+else:
+    df = pd.read_excel(inputfile, engine='openpyxl')
 
 # Rename the first column to "Index"
 df.rename(columns={df.columns[0]: "Index"}, inplace=True)
@@ -115,6 +128,8 @@ df_2 = df_incomplete_ordered
 
 #%% apply xgb to 
 import xgboost as xgb
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
 #! the hyperparameters are objective, n_estimators, learning_rate, random_state
 
 # %% Seperate the dataset with dependents unknown and dependents known
@@ -138,11 +153,36 @@ y = df_with_MonthlyIncome['MonthlyIncome']  # Target
 # Splitting the data with known MonthlyIncome for training and validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize XGBoost regressor
-xgb_regressor = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=1000, learning_rate=0.05, random_state=42)
+param_grid = {
+    'n_estimators': [100, 500, 1000],
+    'learning_rate': [0.01, 0.05, 0.1],
+    'max_depth': [3, 5, 7],
+    'colsample_bytree': [0.7, 0.8, 1.0],
+    'subsample': [0.7, 0.8, 1.0]
+}
 
-# Train the model
-xgb_regressor.fit(X_train, y_train, early_stopping_rounds=10, eval_set=[(X_test, y_test)], verbose=False)
+#! max_depth: Maximum Depth of a Tree: The depth of a tree is the length of the longest path from the root node down to a leaf node. A tree with a depth of 3 means that there are at most three levels of nodes, including the root node.
+#! 
+
+grid_search = GridSearchCV(
+    estimator=xgb.XGBRegressor(objective='reg:squarederror', random_state=42),
+    param_grid=param_grid,
+    scoring='neg_mean_squared_error',
+    cv=3,
+    verbose=2,
+    n_jobs=-1
+)
+
+#%%grid_search fit
+
+grid_search.fit(X_train, y_train)
+
+#%%
+
+# Best parameter set
+print("Best parameters found: ", grid_search.best_params_)
+
+xgb_regressor =  grid_search.best_estimator_
 
 # Predict on the test set
 y_pred = xgb_regressor.predict(X_test)
@@ -150,6 +190,32 @@ y_pred = xgb_regressor.predict(X_test)
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Squared Error: {mse}')
+
+# Calculate Mean Absolute Error
+mae = mean_absolute_error(y_test, y_pred)
+print("Mean Absolute Error:", mae)
+
+# Calculate R-squared
+r2 = r2_score(y_test, y_pred)
+print("R-squared:", r2)
+#%% save the parameters
+
+from joblib import dump
+# Save the model
+dump(xgb_regressor, regressor_filename)
+# Save the best parameters
+dump(grid_search.best_params_, xgb_train_parameter_filename)
+
+#%% Apply previously trained model
+
+#from joblib import load
+
+# Load the model
+#xgb_regressor = load('xgb_regressor.joblib')
+
+# Load the best parameters
+#best_params = load('best_params.joblib')
+
 
 # %% 
 # Prepare df_missing_dependents for prediction
@@ -187,3 +253,5 @@ new_order =  [col for col in df]
 df_3 = df_3[new_order]
 
 df_3.to_excel("Imputed_data.xlsx", index=False)
+
+# %%
