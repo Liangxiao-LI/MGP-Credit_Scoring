f103c9177b73ffa3d6b22838b7ea92727629b06f
BL-Starlord <1021618717@qq.com>
2024-02-21 22:17:19 +0000

    Applied transformation on monthly income into the code

--- Credit_score_Imputation.py
+++ Credit_score_Imputation.py
@@ -2,9 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-#!Row65 change randomforest hyperparameter
-#!Row 141 change XGB hyperparameter
+import numpy as np
 #%% Input/Output modification
 #! Change all these before running the code
 #The filename for the data
@@ -46,36 +44,20 @@ for column in columns_with_missing_values:
     plt.title(f'Boxplot of {column}')
     plt.show()
 
-
 # %% Predict NumberOfDependents column using the dataset without MonthlyIncome and SeriousDlqin2yrs
 
-# create a dataset for predicting NumberOfDependents
-df_1 = df.drop(columns=[ 'MonthlyIncome','SeriousDlqin2yrs'])
+df_1 = df.drop(columns=[ 'MonthlyIncome','SeriousDlqin2yrs']) # create a dataset for predicting NumberOfDependents
 
-# Check for missing values in each column
-#missing_values = df_1.isna().sum()
-#print(missing_values)
+#Seperate the dataset with dependents unknown and dependents known
+df_with_dependents = df_1.dropna(subset=['NumberOfDependents']) # DataFrame with rows where NumberOfDependents is not NA
 
-# %% Seperate the dataset with dependents unknown and dependents known
+df_missing_dependents = df_1[df_1['NumberOfDependents'].isna()] # DataFrame with rows where NumberOfDependents is NA
 
-# DataFrame with rows where NumberOfDependents is not NA
-df_with_dependents = df_1.dropna(subset=['NumberOfDependents'])
-#missing_values = df_with_dependents.isna().sum()
-#print(missing_values)
-
-# DataFrame with rows where NumberOfDependents is NA
-df_missing_dependents = df_1[df_1['NumberOfDependents'].isna()]
-#missing_values = df_missing_dependents.isna().sum()
-#print(missing_values)
-
-#%% start training the random forest
-#! The hyperparameter is random_state
-
+#%% Import model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import GridSearchCV
-
 import xgboost as xgb
 from joblib import load,dump #?used for saving model hyperparameters
 
@@ -94,12 +76,20 @@ y = df_with_dependents['NumberOfDependents']  # Target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 param_grid = {
-    'n_estimators': [400,500],
+    'n_estimators': [500],
     'learning_rate': [0.01],
-    'max_depth': [3, 5],
-    'colsample_bytree': [ 0.8,1.0],
-    'subsample': [0.6, 0.7]
+    'max_depth': [3],
+    'colsample_bytree': [ 0.8],
+    'subsample': [0.6]
 }
+
+#param_grid = {
+#    'n_estimators': [400,500],
+#    'learning_rate': [0.01],
+#    'max_depth': [3, 5],
+#    'colsample_bytree': [ 0.8,1.0],
+#    'subsample': [0.6, 0.7]
+#}
 
 #! max_depth: Maximum Depth of a Tree: The depth of a tree is the length of the longest path from the root node down to a leaf node. A tree with a depth of 3 means that there are at most three levels of nodes, including the root node.
 #! The colsample_bytree parameter in XGBoost is a hyperparameter that specifies the fraction of features (columns) to be randomly sampled for each tree. Before constructing each tree in the model, XGBoost randomly selects a subset of the features based on the colsample_bytree value. This is part of the model's built-in feature selection method to prevent overfitting and to add more randomness to the model fitting process.
@@ -143,15 +133,14 @@ print("Old Model R-squared:", r2)
 
 
 #%% save the trained model
-
+xgb_regressor = xgb_regressor_prev # use previous model
 # Save the model
 #dump(xgb_regressor, regressor_NOD_filename)
 # Save the best parameters
 #dump(grid_search.best_params_, xgb_train_parameter_NOD_filename)
 
-# %% 
+#%% Prepare df_missing_dependents for prediction
 
-# Prepare df_missing_dependents for prediction
 # Ensure to drop the 'NumberOfDependents' column if it hasn't been dropped already
 X_missing = df_missing_dependents.drop(['NumberOfDependents','Index'], axis=1)
 
@@ -172,15 +161,7 @@ df_incomplete_unordered = pd.concat([df_with_dependents, df_imputed])
 
 # Sort df_complete by the "Index" column
 df_incomplete_ordered = df_incomplete_unordered.sort_values(by="Index")
-
-# Check for missing values in each column
-#missing_values = df_complete_ordered.isna().sum()
-#print(missing_values)
-
 df_incomplete_ordered['MonthlyIncome'] = df.set_index('Index')['MonthlyIncome'].loc[df_incomplete_ordered['Index']].values
-
-#missing_values = df_complete_ordered.isna().sum()
-#print(missing_values)
 
 df_2 = df_incomplete_ordered
 
@@ -193,27 +174,35 @@ df_missing_MonthlyIncome = df_2[df_2['MonthlyIncome'].isna()] # DataFrame with r
 X = df_with_MonthlyIncome.drop(['MonthlyIncome','Index'], axis=1)  # Features
 y = df_with_MonthlyIncome['MonthlyIncome']  # Target
 
+y[y <= 0] = 0.01 # replace all values that are 0 into 0.01
+y = np.log(y)
+
 #%% Load previously trained model
 xgb_regressor_prev = load(regressor_MI_filename)
 xgb_best_parameters = load(xgb_train_parameter_MI_filename)
 
 print(f"The hyperparameters for the previous model are: {xgb_best_parameters}")
 
-
-
 #%% xgb model training
 # Splitting the data with known MonthlyIncome for training and validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-y_pred_prev = xgb_regressor_prev.predict(X_test)
 
 param_grid = {
-    'n_estimators': [100, 500, 1000],
-    'learning_rate': [0.01, 0.05, 0.1],
-    'max_depth': [3, 5, 7],
-    'colsample_bytree': [0.7, 0.8, 1.0],
-    'subsample': [0.7, 0.8, 1.0]
+    'n_estimators': [500],
+    'learning_rate': [0.01],
+    'max_depth': [5],
+    'colsample_bytree': [ 1.0],
+    'subsample': [0.7]
 }
+
+#param_grid = {
+#    'n_estimators': [100, 500, 1000],
+#    'learning_rate': [0.01, 0.05, 0.1],
+#    'max_depth': [3, 5, 7],
+#    'colsample_bytree': [0.7, 0.8, 1.0],
+#    'subsample': [0.7, 0.8, 1.0]
+#}
 
 #! max_depth: Maximum Depth of a Tree: The depth of a tree is the length of the longest path from the root node down to a leaf node. A tree with a depth of 3 means that there are at most three levels of nodes, including the root node.
 #! The colsample_bytree parameter in XGBoost is a hyperparameter that specifies the fraction of features (columns) to be randomly sampled for each tree. Before constructing each tree in the model, XGBoost randomly selects a subset of the features based on the colsample_bytree value. This is part of the model's built-in feature selection method to prevent overfitting and to add more randomness to the model fitting process.
@@ -237,6 +226,7 @@ xgb_regressor =  grid_search.best_estimator_
 
 #%% Predict on the test set
 y_pred = xgb_regressor.predict(X_test)
+y_pred_prev = xgb_regressor_prev.predict(X_test)
 
 #%%
 # Evaluate the trained model
@@ -256,9 +246,9 @@ print("Old Model R-squared:", r2)
 #%% save the parameters 
 #! only save it if you are 100% sure it's better than previous model
 # Save the model
-#dump(xgb_regressor, regressor_MI_filename)
+dump(xgb_regressor, regressor_MI_filename)
 # Save the best parameters
-#dump(grid_search.best_params_, xgb_train_parameter_MI_filename)
+dump(grid_search.best_params_, xgb_train_parameter_MI_filename)
 
 
 
@@ -268,7 +258,7 @@ print("Old Model R-squared:", r2)
 X_missing = df_missing_MonthlyIncome.drop(['MonthlyIncome','Index'], axis=1)
 
 # Use the trained model to predict 'NumberOfDependents' for the missing values dataset
-predicted_MonthlyIncome = xgb_regressor.predict(X_missing)
+predicted_MonthlyIncome = np.exp(xgb_regressor.predict(X_missing))
 
 # You can then add these predictions back to df_missing_dependents if you want to fill the missing values
 df_missing_MonthlyIncome['MonthlyIncome'] = predicted_MonthlyIncome
